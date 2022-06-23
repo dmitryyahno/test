1cec94b55d9afb207b5c586f4eb3c74e70a96c3c
kleysoul <silk.chudinovski@gmal.com>
2022-06-23 17:47:25 +0300

    first commi

---
+++ test2/struct.h
@@ -0,0 +1,25 @@
+/*
+ * struct.h
+ *
+ *  Created on: 23 черв. 2022 р.
+ *      Author: yahno
+ */
+
+#ifndef STRUCT_H_
+#define STRUCT_H_
+
+#include<string>
+using namespace std;
+struct MP3Tag
+{
+	string Artist;
+	string Title;
+	string Album;
+	string Year;
+	string Comment;
+};
+
+
+
+
+#endif /* STRUCT_H_ */
